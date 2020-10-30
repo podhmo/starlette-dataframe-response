95151489f18d1b4343b84ab73f073e83e0dedc34
podhmo <ababjam61+github@gmail.com>
2020-10-30 23:34:02 +0900

    test: more tests

--- starlette_dataframe_response/tests/test_response.py
+++ starlette_dataframe_response/tests/test_response.py
@@ -12,7 +12,7 @@ def _makeOne(df: pd.DataFrame, **kwargs: t.Dict[str, t.Any]) -> DataFrameRespons
     return DataFrameResponse(df, **kwargs)
 
 
-def test_json():
+def test_json__with_orient_default():
     df = pd.DataFrame({"name": ["foo", "bar"], "age": [20, 0]})
     got = _makeOne(df).body
     want = b'[{"name":"foo","age":20},{"name":"bar","age":0}]'
@@ -30,4 +30,22 @@ def test_json__with_orient_records():
     df = pd.DataFrame({"name": ["foo", "bar"], "age": [20, 0]})
     got = _makeOne(df, to_json_orient="records").body
     want = b'[{"name":"foo","age":20},{"name":"bar","age":0}]'
+    assert want == got
+
+
+def test_json__with_date_format_default():
+    from datetime import date
+
+    df = pd.DataFrame({"now": [date(2000, 1, 1)]})
+    got = _makeOne(df).body
+    want = b'[{"now":946684800000}]'
+    assert want == got
+
+
+def test_json__with_date_format_iso():
+    from datetime import date
+
+    df = pd.DataFrame({"now": [date(2000, 1, 1)]})
+    got = _makeOne(df, to_json_date_format="iso").body
+    want = b'[{"now":"2000-01-01T00:00:00.000Z"}]'
     assert want == got
