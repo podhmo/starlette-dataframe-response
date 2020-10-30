e1798cb11d2105e84d8e91d1183a12d525a9c017
podhmo <ababjam61+github@gmail.com>
2020-10-30 23:36:02 +0900

    chore: add dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 
-install_requires = []
+install_requires = ["starlette", "pandas"]
 dev_requires = ["black", "flake8", "mypy"]
-tests_requires = ["pytest"]
-
+tests_requires = ["pytest", "tabulate"]
+full_requires = [
+    "tabulate",  # for to_markdown()
+]
 setup(
     classifiers=[
         # "License :: OSI Approved :: MIT License",
@@ -15,14 +17,18 @@ setup(
         #   5 - Production/Stable
         "Development Status :: 3 - Alpha",
     ],
-    python_requires=">3.5",
+    python_requires=">3.7",
     packages=find_packages(exclude=["starlette_dataframe_response.tests"]),
     install_requires=install_requires,
-    extras_require={"testing": tests_requires, "dev": dev_requires},
+    extras_require={
+        "testing": tests_requires,
+        "dev": dev_requires,
+        "full": full_requires,
+    },
     tests_require=tests_requires,
     test_suite="starlette_dataframe_response.tests",
-#     entry_points="""
-#       [console_scripts]
-#       starlette-dataframe-response = starlette_dataframe_response.cli:main
-# """,
+    #     entry_points="""
+    #       [console_scripts]
+    #       starlette-dataframe-response = starlette_dataframe_response.cli:main
+    # """,
 )
